c011c1c5defbe89d748453ea6c89ec7a6a062038
carolapedrosa <42421385+carolapedrosa@users.noreply.github.com>
2018-11-25 17:23:38 -0800

    Delete Rules.h

--- Rules.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Player.h"
-bool isMoveValid();
-bool isTokenAvailable(Player * player,  Coordinates);
-bool canBuildTown(Player *, Coordinates);
-bool canBuildCity(Player *, Coordinates);
-bool canBuildRoad(Player *, Coordinates);
-// bool canBuyDevelopment(Player *, Coordinates);
